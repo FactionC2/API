3c9b727e7992235eca3fc17acae0416c18d5bf9c
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-09-18 07:48:31 -0700

    terrible fix for cors changes in socketio pkgs. fixed payload sql schema issue

--- app.py
+++ app.py
@@ -102,7 +102,9 @@ def create_app():
     app.session_interface = ApiSessionInterface()
 
     log("app.py:CreateApp", "Setting up socketio..")
-    socketio.init_app(app, host='0.0.0.0', manage_session=False, message_queue=RABBIT_URL, channel="ConsoleMessages")
+    # You can add extra logging around socketio by setting the following options here: logger=True, engineio_logger=True
+    socketio.init_app(app, host='0.0.0.0', manage_session=False, message_queue=RABBIT_URL, channel="ConsoleMessages",
+                      cors_allowed_origins="*")
 
     from backend.rabbitmq import rabbit_consumer
     rabbit_consumer.socketio = socketio
@@ -114,8 +116,8 @@ def create_app():
     log("app.py:CreateApp", "Finished.")
     return app
 
-app = create_app()
 
+app = create_app()
 
 if __name__ == '__main__':
     log("app.py:main", "main starting...")
