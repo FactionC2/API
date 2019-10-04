b4445ecedfbad4d7fc31fde1df28dc9558d99dc7
3lpsy <elpsy@protonmail.com>
2019-10-03 20:45:08 -0500

    Improve errors returned when loading config values from environment

--- config.py
+++ config.py
@@ -2,16 +2,21 @@ import os
 import json
 from logger import log
 
-config_file_path = '/opt/faction/global/config.json'
+config_file_path = "/opt/faction/global/config.json"
 
 
 def get_config():
-    try:
-        if os.path.exists(config_file_path):
+    if os.path.exists(config_file_path):
+        try:
             with open(config_file_path) as f:
                 config = json.load(f)
             return config
-        else:
+        except Exception as e:
+            log("config.py", f"Error: {str(e)} - {str(type(e))}")
+            log("config.py", "Could not load config file: {0}".format(config_file_path))
+            exit(1)
+    else:
+        try:
             config = dict()
             config["API_UPLOAD_DIR"] = os.environ["API_UPLOAD_DIR"]
             config["FLASK_SECRET"] = os.environ["FLASK_SECRET"]
@@ -23,9 +28,12 @@ def get_config():
             config["RABBIT_PASSWORD"] = os.environ["RABBIT_PASSWORD"]
             config["RABBIT_HOST"] = os.environ["RABBIT_HOST"]
             return config
-    except:
-        log("config.py", "Could not load config file: {0}".format(config_file_path))
-        exit(1)
+        except KeyError as e:
+            log("config.py", "Config value not  in environment: {0}".format(str(e)))
+            exit(1)
+        except Exception as e:
+            log("config.py", "Unknown error: {0}".format(str(e)))
+            exit(1)
 
 
 FACTION_CONFIG = get_config()
@@ -33,10 +41,14 @@ SECRET_KEY = FACTION_CONFIG["FLASK_SECRET"]
 RABBIT_HOST = FACTION_CONFIG["RABBIT_HOST"]
 RABBIT_USERNAME = FACTION_CONFIG["RABBIT_USERNAME"]
 RABBIT_PASSWORD = FACTION_CONFIG["RABBIT_PASSWORD"]
-RABBIT_URL = 'amqp://{}:{}@{}:5672/%2F'.format(RABBIT_USERNAME, RABBIT_PASSWORD, RABBIT_HOST)
+RABBIT_URL = "amqp://{}:{}@{}:5672/%2F".format(
+    RABBIT_USERNAME, RABBIT_PASSWORD, RABBIT_HOST
+)
 DB_USER = FACTION_CONFIG["POSTGRES_USERNAME"]
 DB_PASSWORD = FACTION_CONFIG["POSTGRES_PASSWORD"]
 DB_HOST = FACTION_CONFIG["POSTGRES_HOST"]
 DB_NAME = FACTION_CONFIG["POSTGRES_DATABASE"]
-DB_URI = 'postgresql://{}:{}@{}/{}?client_encoding=utf8'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_URI = "postgresql://{}:{}@{}/{}?client_encoding=utf8".format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+)
 UPLOAD_DIR = FACTION_CONFIG["API_UPLOAD_DIR"]
